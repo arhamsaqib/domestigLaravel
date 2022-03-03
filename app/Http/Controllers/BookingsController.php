bdfefa1bf608c4183e168938eafcc4ac452571f9
Muhammad Arham Saqib <arhamsaqib@outlook.com>
2022-03-03 17:37:59 +0500

    updated booking controller (validation)

--- app/Http/Controllers/BookingsController.php
+++ app/Http/Controllers/BookingsController.php
@@ -10,36 +10,25 @@ use App\Models\Bookings;
 class BookingsController extends Controller
 {
     public function store(Request $request){
-        $request->validate([
-            'customer_id' => 'required|string|max:255',
-            'provider_id' => 'required|string|max:255',
-            'schedule' => 'required|string',
-            'date' => 'sometimes|string',
-            'time' => 'sometimes|string',
-            'payment_type' => 'sometimes|string',
-            'instructions' => 'sometimes|string',
-            'instructions_image' => 'sometimes|string',
-            'latitude' => 'sometimes|string',
-            'longitude' => 'sometimes|string',
-            'location' => 'sometimes|string',
+        $data = $request->validate([
+            'customer_id' => 'required|max:255',
+            'provider_id' => 'required|max:255',
+            'schedule' => 'required',
+            'date' => 'sometimes',
+            'time' => 'sometimes',
+            'payment_type' => 'sometimes',
+            'instructions' => 'sometimes',
+            'instructions_image' => 'sometimes',
+            'latitude' => 'sometimes',
+            'longitude' => 'sometimes',
+            'location' => 'sometimes',
             'status' => 'sometimes|string',
         ]);
 
-        $new= Bookings::create([
-            'customer_id' => $request->customer_id,
-            'provider_id' => $request->provider_id,
-            'schedule' => $request->schedule,
-            'date' => $request->date,
-            'time' => $request->time,
-            'payment_type' => $request->payment_type,
-            'longitude' => $request->longitude,
-            'latitude' => $request->latitude,
-            'location' => $request->location,
-            'instructions' => $request->instructions,
-            'instructions_image' => $request->instructions_image,
-            //'status' => 'active',
-        ]);
+        $collection = collect($data)->filter()->all();
+        $new = Bookings::create($collection);
         return $new;
+     
     }
     public function show($id){
 
